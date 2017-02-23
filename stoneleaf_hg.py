8b15dd2b93d442d95d38153af056ca6cea6bc588
Ethan Furman <ethan@stoneleaf.us>
2017-02-23 10:34:16 -0800

    add g+w command to hg update

--- stoneleaf_hg.py
+++ stoneleaf_hg.py
@@ -1,4 +1,3 @@
-import sys
 from scription import Execute, echo, error
 
 def update_owner(ui, repo, **kwds):
@@ -10,6 +9,19 @@ def update_owner(ui, repo, **kwds):
         home = url[5:]
         print 'using home of', repr(home)
         job = Execute('/usr/local/bin/hgupdate set-owner-of %s' % home)
+        echo(job.stdout, end='')
+        error(job.stderr, end='')
+        return job.returncode
+
+def update_permissions(ui, repo, **kwds):
+    '''
+    add group write permission bit
+    '''
+    url = repo.url()
+    if url.startswith('file:'):
+        home = url[5:]
+        print 'using home of', repr(home)
+        job = Execute('/usr/local/bin/hgupdate add-group-write-to %s' % home)
         echo(job.stdout, end='')
         error(job.stderr, end='')
         return job.returncode
